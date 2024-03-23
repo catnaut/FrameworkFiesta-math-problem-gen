77425b3b2b9a332d6bd447f0d7a3447f2c67379f
catnaut <catnaut@outlook.com>
2024-03-23 15:10:21 +0800

    feat(file): add csv to set parse

--- src/scripts/files.js
+++ src/scripts/files.js
@@ -1,4 +1,5 @@
 import { useFileSystemAccess } from '@vueuse/core'
+import Papa from 'papaparse'
 
 const DATATYPE = 'Text'
 
@@ -6,9 +7,10 @@ const res = {
   dataType: DATATYPE,
   types: [
     {
-      description: 'text',
+      description: 'csv file',
       accept: {
-        'text/plain': ['.txt', '.html']
+        // 'text/plain': []
+        'text/csv': ['.csv']
       }
     }
   ],
@@ -33,6 +35,27 @@ const Files = {
     // TODO: async
     this.problem.save()
     this.answer.save()
+  },
+  /**
+   * 传入一个文件对象，解析文件内容，返回问题集合
+   * @param {Ref} FileObject
+   * @returns {Set} ProblemSet
+   */
+  csvToSet: async (FileObject) => {
+    let result
+    try {
+      console.log(FileObject)
+      result = await Papa.parse(FileObject.data.value, {
+        header: false
+      })
+    } catch (error) {
+      // TODO: Error handling
+      console.error(error)
+    }
+    // TODO: 实现加载动画
+    let data = result.data.slice(1) // 去掉表头
+    data = data.filter((item) => item.length >= 2).map((item) => item.slice(0, 2)) // 去掉空行和多余的数据
+    return new Set(data) // 返回问题集合
   }
 }
 
